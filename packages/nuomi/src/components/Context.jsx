1e37e542e529ffe325c85d5d4d3ad4b81e0f9a09
yinjiazeng <yinjiazeng@163.com>
2024-04-27 16:46:02 +0800

    update

--- packages/nuomi/src/components/Context.jsx
+++ packages/nuomi/src/components/Context.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const RouterContext = React.createContext();
+const RouterContext = React.createContext({});
 RouterContext.displayName = 'RouterContext';
 
-const NuomiContext = React.createContext();
+const NuomiContext = React.createContext({});
 NuomiContext.displayName = 'NuomiContext';
 
 export { RouterContext, NuomiContext };
